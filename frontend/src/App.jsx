4b78383e7c4d3923d7c5ab88b924ce49ecaaf4d4
Rudra <goutam.prasad.2707@gmail.com>
2025-05-01 13:48:21 +0530

    Refactor App and main components to implement routing and context provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import { Footer, Navbar } from "./Components";
+import {
+	Auth,
+	Dashboard,
+	Landing,
+	SnippetDetails,
+	SnippetEditor,
+} from "./Pages";
 
 function App() {
-	return <div>App</div>;
+	return (
+		<div className="w-full h-full bg-primary text-secondary font-raleway">
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Landing />} />
+				<Route path="/auth" element={<Auth />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+				<Route path="/snippet" element={<SnippetDetails />} />
+				<Route path="/editor" element={<SnippetEditor />} />
+			</Routes>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ContextProvider from "./Contexts/context.jsx";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+	<BrowserRouter>
+		<ContextProvider>
+			<App />
+		</ContextProvider>
+	</BrowserRouter>
+);
