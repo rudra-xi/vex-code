ff7edeb6aa0dfff44fc9e4dfb74d90b751b19045
Rudra <goutam.prasad.2707@gmail.com>
2025-05-01 22:08:48 +0530

    Add user state management to ContextProvider

--- frontend/src/Contexts/Context.jsx
+++ frontend/src/Contexts/Context.jsx
@@ -1,8 +1,9 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const Context = createContext();
 
 function ContextProvider(props) {
+	const [user, setUser] = useState(true);
 	const value = {};
 
 	return <Context.Provider value={value}>{props.children}</Context.Provider>;
