31888d7c84312d57e32c5dbd6c6d2ef49d3c2939
Rudra <goutam.prasad.2707@gmail.com>
2025-05-03 00:51:09 +0530

    Refactor navigation and footer links with icons for improved structure and clarity

--- frontend/src/Constants/Constants.jsx
+++ frontend/src/Constants/Constants.jsx
@@ -1,4 +1,55 @@
+// In your Constants/Constants.js
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
+import { MdDashboard, MdSnippetFolder } from "react-icons/md";
+import { LiaConnectdevelop } from "react-icons/lia";
+
 export const navLinks = [
-	{ text: "Dashboard", link: "/dashboard" },
-	{ text: "New Snippet", link: "/snippet" },
+	{
+		text: "Dashboard",
+		link: "/dashboard",
+		icon: <MdDashboard className="inline-block mr-2" />,
+	},
+	{
+		text: "New Snippet",
+		link: "/editor",
+		icon: <MdSnippetFolder className="inline-block mr-2" />,
+	},
+];
+
+export const footerLinks = [
+	{
+		text: "Privacy Policy",
+		link: "/privacy",
+	},
+	{
+		text: "Terms of Service",
+		link: "/terms",
+	},
+	{
+		text: "Contact",
+		link: "/contact",
+	},
+];
+
+export const footerSocialLinks = [
+	{
+		name: "GitHub",
+		icon: <FaGithub />,
+		link: "https://github.com/rudra-xi",
+	},
+	{
+		name: "LinkedIn",
+		icon: <FaLinkedin />,
+		link: "https://www.linkedin.com/in/goutam-rudraxi",
+	},
+	{
+		name: "Instagram",
+		icon: <FaInstagram />,
+		link: "https://www.instagram.com/rudra.xii/",
+	},
+	{
+		name: "Portfolio",
+		icon: <LiaConnectdevelop />,
+		link: "https://rudra-xi-protfolio.netlify.app/",
+	},
 ];
