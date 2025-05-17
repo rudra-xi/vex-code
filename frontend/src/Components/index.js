0c52ddf6a732a5cadf813cd5f4bc2fe554f82229
Rudra <goutam.prasad.2707@gmail.com>
2025-05-17 21:09:10 +0530

    Update component exports in index.js files to include SnippetCard and PinnedFrag

--- frontend/src/Components/index.js
+++ frontend/src/Components/index.js
@@ -4,5 +4,16 @@ import Footer from "./Footer";
 import Contact from "./Contact";
 import ErrorSVG from "./ErrorSVG";
 import TitleCard from "./TitleCard";
+import SnippetCard from "./SnippetCard";
+import SearchBar from "./SearchBar";
 
-export { Loader, TitleCard, Navbar, Footer, Contact, ErrorSVG };
+export {
+	Loader,
+	TitleCard,
+	Navbar,
+	Footer,
+	Contact,
+	ErrorSVG,
+	SnippetCard,
+	SearchBar,
+};
--- frontend/src/Pages/index.js
+++ frontend/src/Pages/index.js
@@ -1,8 +1,8 @@
 import Dashboard from "./Dashboard";
 import Landing from "./Landing";
 import SnippetDetails from "./SnippetDetails";
-import SnippetEditor from "./SnippetEditor";
+import PinnedFrag from "./PinnedFrag";
 import Auth from "./Auth";
 import Error from "./Error";
 
-export { Dashboard, Landing, SnippetDetails, SnippetEditor, Auth, Error };
+export { Dashboard, Landing, SnippetDetails, PinnedFrag, Auth, Error };
